1b4c2ae86f27d445294a02230878b185aa5f4e9b
Somesh Bachani <somesh.bachani.0512@gmail.com>
2020-07-20 10:05:58 +0530

    Initialised Image view as a function for cost effectiveness. Used lateinit so that the need for mentioning null value for it is not required. lateinit used so that the variable will be intialised before calling any function for it.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,6 +10,8 @@ import kotlin.random.Random.Default
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         rollbutton.setOnClickListener {
             rollDice()
         }
+
+        diceImage=findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -31,9 +35,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-
-        val diceImage : ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResourse)
 
         Toast.makeText(this,"Dice Rolled!",Toast.LENGTH_SHORT).show()
